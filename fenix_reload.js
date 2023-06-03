db2b850ffd8395dec73bb91071ece91166bbc820
Ḟenix෴ᙞinario <fbamperius@gmail.com>
2023-06-03 09:14:52 +0200

    console

---
+++ fenix_reload.js
@@ -0,0 +1,7 @@
+
+//Autor: fenixbinario.com
+(function(){
+    console.log("%c paywithalike.com", "font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 #8139df , 15px 15px 0 #00b0ff , 18px 18px 0 rgb(255, 64, 225) , 21px 21px 0 #ffaeb0; margin-bottom: 18px; padding: 5%");  
+
+}
+)();
